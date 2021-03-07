523fcbc2ca13a24e73a5f2476459567e622dea13
ckgathi <ckgathi@users.noreply.github.com>
2021-03-07 19:05:37 +0200

    create appointment calender events

--- edc_appointment/models/appointment.py
+++ edc_appointment/models/appointment.py
@@ -1,12 +1,13 @@
 from edc_base.model_managers import HistoricalRecords
 from edc_base.model_mixins import BaseUuidModel
 from edc_base.sites import CurrentSiteManager, SiteModelMixin
+from edc_calender.models import UpdatesOrCreatesCalenderEventModelMixin
 
 from ..managers import AppointmentManager
 from ..model_mixins import AppointmentModelMixin
 
 
-class Appointment(AppointmentModelMixin, SiteModelMixin, BaseUuidModel):
+class Appointment(AppointmentModelMixin, UpdatesOrCreatesCalenderEventModelMixin, SiteModelMixin, BaseUuidModel):
 
     on_site = CurrentSiteManager()
 
@@ -21,6 +22,16 @@ class Appointment(AppointmentModelMixin, SiteModelMixin, BaseUuidModel):
                 self.visit_code,
                 self.visit_code_sequence)
     natural_key.dependencies = ['sites.Site']
+
+    @property
+    def event_options(self):
+        """Returns the dict of the following attrs
+        `title` `description` `start_time`  `end_time`.
+        """
+        return {
+            'description': self.appt_reason,
+            'start_time': self.appt_datetime,
+            'end_time': self.appt_datetime,}
 
     class Meta(AppointmentModelMixin.Meta):
         pass
